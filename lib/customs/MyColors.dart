2cdaeae3388090d49e49c81867789f356f4fdd61
Michel <michel@mbp-de-michel.home>
2022-06-05 12:24:28 +0200

    reparation code

---
+++ lib/customs/MyColors.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class MyColors {
+  /*static const MaterialColor appColor = MaterialColor(0xffFFAF9A, <int, Color>{
+    50: Color(0xffFFAF9A),
+    100: Color(0xffFF8877),
+    200: Color(0xffE46254),
+    300: Color(0xffB83C32),
+    400: Color(0xff8C0E13)
+  });*/
+  static const Color beige = Color(0xffFFAF9A);
+  static const Color rosePale = Color(0xffFF8877);
+  static const Color roseDark = Color(0xffE46254);
+  static const Color brown = Color(0xffB83C32);
+  static const Color bordeaux = Color(0xff8C0E13);
+}
--- lib/detail_cocktail.dart
+++ lib/detail_cocktail.dart
@@ -2,6 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter/widgets.dart';
+import 'package:saucey/customs/MyColors.dart';
 
 class Detail extends StatelessWidget {
   Detail({Key? key}) : super(key: key);
@@ -27,212 +28,216 @@ class Detail extends StatelessWidget {
                 ),
               ],
             ),
-            Container(
-              padding: EdgeInsets.only(left: 15, right: 15),
-              child: Column(
-                children: [
-                  Container(
-                    height: 40,
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      children: [
-                        Container(
-                          height: 20,
-                          width: 66,
-                          decoration: BoxDecoration(
-                            color: Colors.beige,
-                            borderRadius: BorderRadius.circular(12),
-                          ),
-                          child: Container(
-                            padding: EdgeInsets.symmetric(
-                                horizontal: 10, vertical: 5),
-                            width: double.infinity,
-                            child: Text(
-                              'Alcoholic',
-                              style:
-                                  TextStyle(fontSize: 11, color: Colors.black),
+            Expanded(
+              child: SingleChildScrollView(
+                padding: EdgeInsets.only(left: 15, right: 15, bottom: 15),
+                scrollDirection: Axis.vertical,
+                child: Column(
+                  children: [
+                    Container(
+                      height: 40,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [
+                          Container(
+                            height: 20,
+                            width: 66,
+                            decoration: BoxDecoration(
+                              color: MyColors.beige,
+                              borderRadius: BorderRadius.circular(12),
                             ),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                  Container(
-                    height: 100,
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      children: [
-                        Text(
-                          "Rosemary Blue",
-                          style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              color: Colors.black,
-                              fontSize: 24),
-                        )
-                      ],
-                    ),
-                  ),
-                  Container(
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      children: [
-                        Text(
-                          "15,30€",
-                          style: TextStyle(color: Colors.black, fontSize: 20),
-                        )
-                      ],
-                    ),
-                  ),
-                  Container(
-                    height: 80,
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      children: [
-                        Text(
-                          "Ingredients",
-                          style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              color: Colors.bordeau,
-                              fontSize: 16),
-                        ),
-                        CustomPaint(
-                          size: Size(20, 20),
-                          painter: MyPainter(),
-                        ),
-                      ],
-                    ),
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    children: [
-                      Container(
-                        width: 100,
-                        height: 70,
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              "Gin",
-                              style: TextStyle(
-                                color: Colors.black,
-                                fontSize: 12,
+                            child: Container(
+                              padding: EdgeInsets.symmetric(
+                                  horizontal: 10, vertical: 5),
+                              width: double.infinity,
+                              child: Text(
+                                'Alcoholic',
+                                style: TextStyle(
+                                    fontSize: 11, color: Colors.black),
                               ),
                             ),
-                            Text(
-                              "Blue Curacao",
-                              style: TextStyle(
-                                color: Colors.black,
-                                fontSize: 12,
-                              ),
-                            )
-                          ],
-                        ),
+                          ),
+                        ],
                       ),
-                      Container(
-                        height: 72,
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              "Rosemary",
-                              style: TextStyle(
+                    ),
+                    Container(
+                      height: 100,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          Text(
+                            "Rosemary Blue",
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold,
                                 color: Colors.black,
-                                fontSize: 12,
-                              ),
-                            ),
-                            Text(
-                              "Tonic water",
-                              style: TextStyle(
-                                color: Colors.black,
-                                fontSize: 12,
-                              ),
-                            )
-                          ],
-                        ),
-                      )
-                    ],
-                  ),
-                  Container(
-                    height: 100,
-                    child: Row(
+                                fontSize: 24),
+                          )
+                        ],
+                      ),
+                    ),
+                    Container(
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [
+                          Text(
+                            "15,30€",
+                            style: TextStyle(color: Colors.black, fontSize: 20),
+                          )
+                        ],
+                      ),
+                    ),
+                    Container(
+                      height: 80,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          Text(
+                            "Ingredients",
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                color: MyColors.bordeaux,
+                                fontSize: 16),
+                          ),
+                          CustomPaint(
+                            size: Size(20, 20),
+                            painter: MyPainter(),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Row(
                       mainAxisAlignment: MainAxisAlignment.start,
                       children: [
                         Container(
+                          width: 100,
+                          height: 70,
                           child: Column(
                             crossAxisAlignment: CrossAxisAlignment.start,
                             children: [
-                              Container(
-                                height: 15,
-                                child: Text(
-                                  "How it's made ?",
-                                  style: TextStyle(
-                                      fontWeight: FontWeight.bold,
-                                      color: Colors.bordeau,
-                                      fontSize: 16),
+                              Text(
+                                "Gin",
+                                style: TextStyle(
+                                  color: Colors.black,
+                                  fontSize: 12,
                                 ),
                               ),
-                              CustomPaint(
-                                size: Size(20, 20),
-                                painter: MyPainter2(),
-                              ),
-                              Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  Text(
-                                    "\u2022 Mettre des glacons",
-                                    style: TextStyle(
-                                      color: Colors.black,
-                                      fontSize: 11,
-                                    ),
-                                  ),
-                                  Text(
-                                    "\u2022 Mettre de la vodka",
-                                    style: TextStyle(
-                                      color: Colors.black,
-                                      fontSize: 11,
-                                    ),
-                                  ),
-                                  Text(
-                                    "\u2022 Rajouter le citron",
-                                    style: TextStyle(
-                                      color: Colors.black,
-                                      fontSize: 11,
-                                    ),
-                                  ),
-                                ],
-                              ),
+                              Text(
+                                "Blue Curacao",
+                                style: TextStyle(
+                                  color: Colors.black,
+                                  fontSize: 12,
+                                ),
+                              )
                             ],
+                          ),
+                        ),
+                        Container(
+                          height: 72,
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                "Rosemary",
+                                style: TextStyle(
+                                  color: Colors.black,
+                                  fontSize: 12,
+                                ),
+                              ),
+                              Text(
+                                "Tonic water",
+                                style: TextStyle(
+                                  color: Colors.black,
+                                  fontSize: 12,
+                                ),
+                              )
+                            ],
+                          ),
+                        )
+                      ],
+                    ),
+                    Container(
+                      height: 100,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          Container(
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Container(
+                                  height: 15,
+                                  child: Text(
+                                    "How it's made ?",
+                                    style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        color: MyColors.bordeaux,
+                                        fontSize: 16),
+                                  ),
+                                ),
+                                CustomPaint(
+                                  size: Size(20, 20),
+                                  painter: MyPainter2(),
+                                ),
+                                Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(
+                                      "\u2022 Mettre des glacons",
+                                      style: TextStyle(
+                                        color: Colors.black,
+                                        fontSize: 11,
+                                      ),
+                                    ),
+                                    Text(
+                                      "\u2022 Mettre de la vodka",
+                                      style: TextStyle(
+                                        color: Colors.black,
+                                        fontSize: 11,
+                                      ),
+                                    ),
+                                    Text(
+                                      "\u2022 Rajouter le citron",
+                                      style: TextStyle(
+                                        color: Colors.black,
+                                        fontSize: 11,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Container(
+                          height: 35,
+                          width: 120,
+                          decoration: BoxDecoration(
+                            color: MyColors.bordeaux,
+                            borderRadius: BorderRadius.circular(15),
+                          ),
+                          child: Container(
+                            padding: EdgeInsets.symmetric(
+                                horizontal: 13, vertical: 10),
+                            width: double.infinity,
+                            child: Text(
+                              'Ajouter au panier',
+                              style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 12,
+                                  color: Colors.white),
+                            ),
                           ),
                         ),
                       ],
                     ),
-                  ),
-                ],
-              ),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Container(
-                  height: 35,
-                  width: 120,
-                  decoration: BoxDecoration(
-                    color: Colors.bordeau,
-                    borderRadius: BorderRadius.circular(15),
-                  ),
-                  child: Container(
-                    padding: EdgeInsets.symmetric(horizontal: 13, vertical: 10),
-                    width: double.infinity,
-                    child: Text(
-                      'Ajouter au panier',
-                      style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 12,
-                          color: Colors.white),
-                    ),
-                  ),
+                  ],
                 ),
-              ],
+              ),
             ),
           ],
         ),
@@ -247,7 +252,7 @@ class MyPainter extends CustomPainter {
     final p1 = Offset(-70, 25);
     final p2 = Offset(300, 25);
     final paint = Paint()
-      ..color = Colors.bordeau
+      ..color = MyColors.bordeaux
       ..strokeWidth = 2;
     canvas.drawLine(p1, p2, paint);
   }
@@ -264,7 +269,7 @@ class MyPainter2 extends CustomPainter {
     final p1 = Offset(10, 10);
     final p2 = Offset(380, 10);
     final paint = Paint()
-      ..color = Colors.bordeau
+      ..color = MyColors.bordeaux
       ..strokeWidth = 2;
     canvas.drawLine(p1, p2, paint);
   }
