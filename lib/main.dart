63237c5dd1908dd18a5e5b37b56f6c737676a4c6
Michel <michel@mbp-de-michel.home>
2022-06-05 20:00:30 +0200

    beginning search

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:saucey/search/views/search.dart';
 
 import 'home/views/home.dart';
 
@@ -12,7 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Home(),
+      home: Search(),
     );
   }
 }
